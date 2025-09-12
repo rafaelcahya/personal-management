9653ae36b68bd022a6025219826312f964684a82
rafael cahya <cahyaputraugira95@gmail.com>
2025-09-12 15:53:15 +0700

    fix sonner and add eslint config

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,25 +1,25 @@
-"use client";
+"use client"
 
-import { useTheme } from "next-themes";
-import { toast as Sonner, ToasterProps } from "sonner";
+import { useTheme } from "next-themes"
+import { Toaster as Sonner, ToasterProps } from "sonner"
 
-const toast = ({ ...props }: ToasterProps) => {
-    const { theme = "system" } = useTheme();
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme()
 
-    return (
-        <Sonner
-            theme={theme as ToasterProps["theme"]}
-            className="toast group"
-            style={
-                {
-                    "--normal-bg": "var(--popover)",
-                    "--normal-text": "var(--popover-foreground)",
-                    "--normal-border": "var(--border)",
-                } as React.CSSProperties
-            }
-            {...props}
-        />
-    );
-};
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  )
+}
 
-export { toast };
+export { Toaster }
