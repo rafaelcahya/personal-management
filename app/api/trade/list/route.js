0c03e357590af3de4cb80685a045a7b2ffbbbce9
rafael cahya <cahyaputraugira95@gmail.com>
2025-09-12 15:50:24 +0700

    Fix typo

--- app/api/trade/list/route.js
+++ app/api/trade/list/route.js
@@ -14,5 +14,4 @@ export async function GET() {
             status: 401,
         });
     }
-}
-``;
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,59 +1,63 @@
 import type { Metadata } from "next";
-import { Geist, Nunito_Sans} from "next/font/google";
+import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { Toaster } from "sonner";
-import "./style/index.css"
-import background from "./assets/background.jpg"
+import "./style/index.css";
+import background from "./assets/background.jpg";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+    metadataBase: new URL(defaultUrl),
+    title: "Next.js and Supabase Starter Kit",
+    description: "The fastest way to build apps with Next.js and Supabase",
 };
 
 const geist = Geist({
-  variable: "--font-geist",
-  display: "swap",
-  subsets: ["latin"],
+    variable: "--font-geist",
+    display: "swap",
+    subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geist.className} antialiased relative min-h-screen`}
-        style={{
-          backgroundImage: `url(${background.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        {/* Overlay frosted glass */}
-        <div className="absolute inset-0 bg-white/80 backdrop-blur-2xl" />
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                className={`${geist.className} antialiased relative min-h-screen`}
+                style={{
+                    backgroundImage: `url(${background.src})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundAttachment: "fixed",
+                }}
+            >
+                {/* Overlay frosted glass */}
+                <div className="absolute inset-0 bg-white/80 backdrop-blur-2xl" />
 
-        {/* Main Content */}
-        <div className="relative z-10">
-          <Toaster richColors position="top-center" />
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </div>
-      </body>
-    </html>
-  );
+                {/* Main Content */}
+                <div className="relative z-10">
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                        <Toaster
+                            richColors
+                            position="top-center"
+                            theme="system"
+                        />
+                    </ThemeProvider>
+                </div>
+            </body>
+        </html>
+    );
 }
