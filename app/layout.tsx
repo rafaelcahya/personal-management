c0e1f81fbb6098a62137922579b90c418a8a1cbe
rafael cahya <cahyaputraugira95@gmail.com>
2025-09-23 20:55:37 +0700

    Update description in metadata for clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata: Metadata = {
     metadataBase: new URL(defaultUrl),
     title: "Trading Performance",
-    description: "The fastest way to build apps with Next.js and Supabase",
+    description: "Trading Performance",
 };
 
 const geist = Quicksand({
