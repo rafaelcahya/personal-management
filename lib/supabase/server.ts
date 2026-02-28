c9928c749b837778a774ca8f12da59cb011da172
rafael cahya <cahyaputraugira95@gmail.com>
2026-02-28 11:29:16 +0700

    setup

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -7,28 +7,32 @@ import { cookies } from "next/headers";
  * it.
  */
 export async function createClient() {
-  const cookieStore = await cookies();
+    const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-  return createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-          cookies: {
-              getAll() {
-                  return cookieStore.getAll();
-              },
-              setAll(cookiesToSet) {
-                  try {
-                      cookiesToSet.forEach(({ name, value, options }) =>
-                          cookieStore.set(name, value, options),
-                      );
-                  } catch {
-                      // The `setAll` method was called from a Server Component.
-                      // This can be ignored if you have middleware refreshing
-                      // user sessions.
-                  }
-              },
-          },
-      },
-  );
+    if (!url || !key) {
+        throw new Error(
+            `Supabase client missing env; URL=${!!url}, KEY=${!!key} -- BUILD TIME`,
+        );
+    }
+
+    const cookieStore = await cookies();
+
+    return createServerClient(url, key, {
+        cookies: {
+            getAll() {
+                return cookieStore.getAll();
+            },
+            setAll(cookiesToSet) {
+                try {
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        cookieStore.set(name, value, options),
+                    );
+                } catch {
+                    // in Server Component
+                }
+            },
+        },
+    });
 }
+
