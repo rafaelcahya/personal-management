febf984379ca6bf19c57c9564e6e7de6bc7a6925
rafael cahya <cahyaputraugira95@gmail.com>
2026-06-15 15:57:56 +0700

    test: running dashboard API coverage (#331)
    Rewrite dashboard-api.cy.js with full coverage across 16 describe
    blocks (55 tests). Fix status oneOf missing resting value. Add qs
    support to getDashboard command. Add getFitnessAgeTrend commands.

--- cypress/support/common/running/api/analytics/commands.js
+++ cypress/support/common/running/api/analytics/commands.js
@@ -3,6 +3,7 @@ import constants from '../../../../../fixtures/app-constants.json'
 const TARGET_EFFORT = constants.endpoints.running_analytics.target_effort
 const PERSONAL_BESTS = constants.endpoints.running_analytics.personal_bests
 const CALORIE_TREND = constants.endpoints.running_analytics.calorie_trend
+const FITNESS_AGE = constants.endpoints.running_analytics.fitness_age
 
 Cypress.Commands.add('getTargetEffort', (qs = {}) => {
   return cy.apiRequestWithSession('GET', TARGET_EFFORT, { qs })
@@ -26,4 +27,12 @@ Cypress.Commands.add('getCalorieTrend', (qs = {}) => {
 
 Cypress.Commands.add('getCalorieTrendNoAuth', () => {
   return cy.apiRequestNoAuth('GET', CALORIE_TREND)
+})
+
+Cypress.Commands.add('getFitnessAgeTrend', () => {
+  return cy.apiRequestWithSession('GET', FITNESS_AGE)
+})
+
+Cypress.Commands.add('getFitnessAgeTrendNoAuth', () => {
+  return cy.apiRequestNoAuth('GET', FITNESS_AGE)
 })
